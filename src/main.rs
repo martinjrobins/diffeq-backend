04e29c9d341d58b0abfa14bcebb6afd658312e78
martinjrobins <martinjrobins@gmail.com>
2023-09-28 21:14:25 +0000

    remove something went wrong

--- src/main.rs
+++ src/main.rs
@@ -76,7 +76,7 @@ impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         (
             StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", self.0),
+            self.0.to_string(),
         )
             .into_response()
     }
